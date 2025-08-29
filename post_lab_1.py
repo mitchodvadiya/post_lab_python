82c556faa91c521babdb6dfd7578c062eb95ba4e
mitchodvadiya <mitchodavadiya@gmail.com>
2025-08-29 11:38:16 +0530

    Update and rename post_lab_1_2_3.py to post_lab_1.py

---
+++ post_lab_1.py
@@ -0,0 +1,27 @@
+#  post lab : 1 
+# experiment no : 1
+# code : 1
+
+s = "welcome to python"
+for i in range(5):
+    print(s[0:17])
+
+
+# code : 2
+
+print("Number\tSquare\tCube")
+for i in range(1, 5):  
+    print(f"{i}\t{i**2}\t{i**3}")
+
+
+# code : 3
+
+numerator = 9.5 * 4.5 - 2.5 * 3
+denominator = 45.5 - 3.5
+
+result = numerator / denominator
+
+print("Result is:", result)
+
+########################################################################################################
+
